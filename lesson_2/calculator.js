f8faf0cb969468843f88350d089836b65f943a06
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-03-18 19:52:09 +0900

    added refactored calculator and eslint

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -6,24 +6,54 @@
 
 let rlSync = require('readline-sync');
 
-console.log('Welcome to Calculator!');
-let number1 = Number(rlSync.question('What is the first number?\n'));
-let number2 = Number(rlSync.question('What is the second number?\n'));
-let operation = rlSync.question('What operation should I perform?\n1) Add 2) Subtract 3) Multiply 4) Divide\n');
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt('Welcome to Calculator!');
+
+prompt("What's the first number?");
+let number1 = rlSync.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = rlSync.question();
+}
+
+prompt("What's the second number?");
+let number2 = rlSync.question();
+
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = rlSync.question();
+}
+
+prompt("What operation would you like to use?\n1) Addition 2) Subtraction 3) Multiplication 4) Division");
+let operation = rlSync.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = rlSync.question();
+}
 
 let answer;
 switch (operation) {
   case '1':
-    answer = number1 + number2;
+    answer = Number(number1) + Number(number2);
     break;
   case '2':
-    answer = number1 - number2;
+    answer = Number(number1) - Number(number2);
     break;
   case '3':
-    answer = number1 * number2;
+    answer = Number(number1) * Number(number2);
     break;
   case '4':
-    answer = number1 / number2;
+    answer = Number(number1) / Number(number2);
+    break;
 }
 
-console.log(`The result is ${answer}.`);
+console.log(`The result is: ${answer}.`);
