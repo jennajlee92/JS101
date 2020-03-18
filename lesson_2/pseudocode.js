fbe17c2b09df6d8cdf2e05297e3bf8520fcbdea2
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-03-18 14:35:22 +0900

    added pseudocode file from lesson 2

---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,43 @@
+/*
+-a function that returns the sum of two numbers-
+
+Casual:
+input 2 numbers; return sum
+add the two numbers within the body of the function
+
+Formal:
+START
+
+GET two numbers in argument of function
+PRINT or RETURN num1 + num2
+
+END
+
+-a function that takes an array of strings and returns
+a string of all the strings concatenated together-
+
+START
+
+GET array of strings
+PRINT or RETURN reduced array of strings into one string
+
+END
+
+-a function that takes an array of integers and returns a new
+array with every other element-
+
+START
+
+GET array of integers
+SET index = 0
+SET new array = []
+
+WHILE index < array of integers.length
+READ array[index] and push the value to the new array
+index += 2
+
+PRINT or RETURN new array
+
+END
+
+*/
