c32c1982304ed3a0a1f58e9062200cb80fe6064d
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-03-22 11:57:42 +0900

    added functions for num to signed number and num to string

--- small_problems/numToString.js
+++ small_problems/numToString.js
@@ -26,3 +26,17 @@ integerToString(4321);      // "4321"
 integerToString(0);         // "0"
 integerToString(5000);      // "5000"
 integerToString(1234567890);      // "1234567890"
+
+function signedIntegerToString(number) {
+  if (number < 0) {
+    return '-' + integerToString(-number);
+  } else if (number === 0) {
+    return '0';
+  } else {
+    return '+' + integerToString(number);
+  }
+}
+
+console.log(signedIntegerToString(4321) === "+4321");
+console.log(signedIntegerToString(-123) === "-123");
+console.log(signedIntegerToString(0) === "0");
