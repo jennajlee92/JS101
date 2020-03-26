00eefd21c1aaf2f63233ead6f53425949f2b387e
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-03-26 21:28:26 +0900

    added hard question 4

--- lesson_3/hard.js
+++ lesson_3/hard.js
@@ -1,1 +1,20 @@
-// Question 3 STUCK!
+// Question 4
+
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+  
+  if (dotSeparatedWords.length !== 4) {
+    return false;
+  }
+
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+isDotSeparatedIpAddress('10.4.5.11');
