b3c45e36b1e806f750dd8ff463197f9d8d8907b3
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-04-17 20:13:33 +0900

    added function to reverse words in a string if words are greater than or equal to 5 characters

--- small_problems/easy_6/reverse.js
+++ small_problems/easy_6/reverse.js
@@ -5,3 +5,18 @@ let reverseSentence = string => {
 reverseSentence('');                       // ""
 reverseSentence('Hello World');            // "World Hello"
 reverseSentence('Reverse these words');    // "words these Reverse"
+
+let reverseWords = string => {
+  let arrayWords = string.split(' ');
+  return arrayWords.map(word => {
+    if (word.length >= 5) {
+      return word.split('').reverse().join('');
+    } else {
+      return word;
+    }
+  }).join(' ');
+}
+
+console.log(reverseWords('Professional'));             // "lanoisseforP"
+console.log(reverseWords('Walk around the block'));    // "Walk dnuora the kcolb"
+console.log(reverseWords('Launch School'));            // "hcnuaL loohcS"
