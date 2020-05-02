254d5fd8cf14e1ae5b312f2be1e5c40f43ff92a6
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-05-02 16:02:18 +0900

    added function to remove vowels in an array of strings

---
+++ small_problems/string_and_text/removeVowels.js
@@ -0,0 +1,12 @@
+/*
+input array of strings
+output array of strings with aeiou removed
+*/
+
+function removeVowels(array) {
+  return array.map(string => string.replace(/[aeiou]/gi, ''));
+}
+
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
