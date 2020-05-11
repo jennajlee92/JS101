e67f6cc1319d69a5466bbfbb24bd48fcf17e8446
Jenna Ji Min Lee <jennajlee92@gmail.com>
2020-05-11 16:09:09 +0900

    added function to replace words with numbers

---
+++ small_problems/medium_1/wordtodigit.js
@@ -0,0 +1,24 @@
+function wordToDigit(string) {
+  const NUM_WORDS = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+  };
+
+  Object.keys(NUM_WORDS).forEach(word => {
+    let regex = new RegExp(word, 'g');
+    string = string.replace(regex, NUM_WORDS[word]);
+  });
+
+  return console.log(string);
+}
+
+wordToDigit('Please call me at five five five one two three four. Thanks.');
+// "Please call me at 5 5 5 1 2 3 4. Thanks."
